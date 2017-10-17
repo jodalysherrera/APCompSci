dc6f473ab1929c3f55bd26ef8edf9f68e0b213f1
Jodalys Herrera <jodalysherrera77@gmail.com>
2017-10-17 13:41:52 -0400

    Create magpi.java

---
+++ magpi.java
@@ -0,0 +1,101 @@
+
+
+public class Magpie2
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, my name is Jody. let's talk.";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if(statement.trim().length()==0){
+			response = "Say something.";
+		}
+		else if (statement.indexOf("no") >= 0)
+		{
+			response = "Why not?";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else if(statement.indexOf("cat")>= 0
+				|| statement.indexOf("dog")>=0)
+		{
+			response = "Tell me more about your pets.";
+		} else if(statement.indexOf("me") >= 0){
+			response = "I'm pretty cool.";
+		} 
+		else if (statement.indexOf("christmas") >= 0)
+		{
+			response = "It's Halloween!";
+		} 
+		else if (statement.indexOf("thanksgiving") >= 0)
+		{
+			response = "I want food.";
+		}
+		else if (statement.indexOf("sofia") >= 0)
+		{
+			response = "What a loser.";
+		}
+		else {
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 6;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "cool.";
+		} 
+		else if (whichResponse == 5)
+		{
+			response = "yo.";
+		}
+
+		return response;
+	}
+}
